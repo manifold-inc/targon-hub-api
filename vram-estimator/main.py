44f491f9190e81b124e7a4d7b219d781f3489cd9
josh <josh@mjbrowns.com>
2024-11-07 15:54:06 -0600

    fix: use multi lib

--- vram-estimator/main.py
+++ vram-estimator/main.py
@@ -1,8 +1,6 @@
 import math
-from pydantic import BaseModel
+from pydantic import BaseModel, Enum
 from fastapi import FastAPI
-
-
 from accelerate.commands import estimate
 
 
@@ -11,34 +9,37 @@ def bytes_to_mib(bytes_value):
     return math.ceil(mib_value)
 
 
-def estimate_max_size(model_name):
+def estimate_max_size(model_name, lib):
     "Returns size in MiB, what nvidia smi prints"
     try:
         model = estimate.create_empty_model(
-            model_name, library_name="transformers", trust_remote_code=False
+            model_name, library_name=lib, trust_remote_code=False
         )
     except (RuntimeError, OSError) as e:
         library = estimate.check_has_model(e)
-        if library != "unknown":
-            raise RuntimeError(
-                f"Tried to load `{model_name}` with `{library}` but a possible model to load was not found inside the repo."
-            )
+        print(
+            f"Tried to load `{model_name}` with `{library}` but a possible model to load was not found inside the repo."
+        )
         return None
 
     total_size, _ = estimate.calculate_maximum_sizes(model)
-    return max(bytes_to_mib(total_size) / 81000)
+    return math.ceil(bytes_to_mib(total_size) / 81000)
 
 
 app = FastAPI()
 
 
+
 class Request(BaseModel):
     model: str
+    library_name: str
 
 
 @app.post("/estimate")
 async def post_estimate(req: Request):
-    return {"required_gpus", estimate_max_size(req.model)}
+    required = estimate_max_size(req.model, req.library_name)
+    print(f"{req.model}: {required}")
+    return {"required_gpus", required}, 200
 
 
 @app.get("/")
