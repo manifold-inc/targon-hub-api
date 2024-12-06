a857995f437dcc6f52e9d742f3b01f6081a88622
josh <josh@mjbrowns.com>
2024-12-06 09:50:28 -0600

    fix: defer sync

--- api/server.go
+++ api/server.go
@@ -79,7 +79,8 @@ func main() {
 	})
 	e.Use(middleware.RecoverWithConfig(middleware.RecoverConfig{
 		StackSize: 1 << 10, // 1 KB
-		LogErrorFunc:  func(c echo.Context, err error, stack []byte) error {
+		LogErrorFunc: func(c echo.Context, err error, stack []byte) error {
+			defer sugar.Sync()
 			sugar.Errorw("Api Panic", "error", err.Error())
 			return c.String(500, "Internal Server Error")
 		},
