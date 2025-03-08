4d38477bb71cd6c07655847e270f72160b7f1855
josh <josh@mjbrowns.com>
2025-03-08 13:54:41 -0600

    feat: use self reported qps per models

--- miner-cache/cache.py
+++ miner-cache/cache.py
@@ -1,6 +1,5 @@
 import asyncio
 import aiohttp
-import uuid
 from typing import List, Tuple
 import os
 import bittensor as bt
@@ -14,44 +13,11 @@ from typing import Any, Dict, List, Optional, Union
 import time
 from substrateinterface import Keypair
 from logconfig import setupLogging
-from cryptography.hazmat.primitives import hashes, serialization
-from cryptography.hazmat.primitives.asymmetric import padding
-import base64
 
 logger = setupLogging()
 
 
-def load_public_key():
-    try:
-        with open("./public_key.pem", "rb") as key_file:
-            public_key = serialization.load_pem_public_key(key_file.read())
-        return public_key
-    except Exception as e:
-        raise Exception(f"Error loading public key: {e}")
-
-
-PUBKEY = load_public_key()
-
-
-def verify_signature(msg: dict, signature: str, public_key):
-    try:
-        msg_bytes = json.dumps(msg, separators=(",", ":")).encode("utf-8")
-
-        signature_bytes = base64.b64decode(signature)
-
-        public_key.verify(
-            signature_bytes,
-            msg_bytes,
-            padding.PKCS1v15(),
-            hashes.SHA256(),
-        )
-
-        return True
-    except Exception:
-        return False
-
-
-async def get_models(session, hotkey, axon) -> List[str]:
+async def get_models(session, hotkey, axon) -> Tuple[Dict[str, int], str]:
     headers = generate_header(hotkey, b"", axon.hotkey)
     try:
         async with session.get(
@@ -60,68 +26,13 @@ async def get_models(session, hotkey, axon) -> List[str]:
             timeout=3,
         ) as res:
             if res.status != 200:
-                return []
+                return {}, f"status code {res.status}"
             models = await res.json()
-            if not isinstance(models, list):
-                return []
-            return models
-    except Exception:
-        return []
-
-
-async def get_gpus(session, hotkey, axon) -> Tuple[int, Optional[Dict], Optional[str]]:
-    nonce = str(uuid.uuid4())
-    req_body = {"nonce": nonce}
-    req_bytes = json.dumps(
-        req_body, ensure_ascii=False, separators=(",", ":"), allow_nan=False
-    ).encode("utf-8")
-    headers = generate_header(hotkey, req_bytes, axon.hotkey)
-    gpus = {"h100": 0, "h200": 0}
-    try:
-        async with session.post(
-            f"http://{axon.ip}:{axon.port}/nodes",
-            headers=headers,
-            data=req_bytes,
-            timeout=aiohttp.ClientTimeout(total=20),
-        ) as res:
-            if res.status != 200:
-                return 0, None, f"Bad status code: {res.status}"
-            nodes = await res.json()
-            if not isinstance(nodes, list):
-                return 0, None, f"response not list"
-            weight = 0
-            for node in nodes:
-                if not isinstance(node, dict):
-                    continue
-                msg = node.get("msg")
-                signature = node.get("signature")
-                if not isinstance(msg, dict):
-                    continue
-                if not isinstance(signature, str):
-                    continue
-                if not verify_signature(msg, signature, PUBKEY):
-                    continue
-                miner_nonce = msg.get("nonce")
-                if miner_nonce != nonce:
-                    continue
-                gpu_info = msg.get("gpu_info", [])
-                if len(gpu_info) == 0:
-                    continue
-                is_h100 = "h100" in gpu_info[0].get("gpu_type", "").lower()
-                is_h200 = "h200" in gpu_info[0].get("gpu_type", "").lower()
-                if not is_h100 and not is_h200:
-                    continue
-                num_gpus = msg.get("no_of_gpus", 0)
-                if is_h100:
-                    weight += 1 * num_gpus
-                    gpus["h100"] += 1 * num_gpus
-                    continue
-                weight += 2 * num_gpus
-                gpus["h200"] += 1 * num_gpus
-
-            return weight, gpus, None
+            if not isinstance(models, dict):
+                return {}, f"Response is not dict"
+            return models, ""
     except Exception as e:
-        return 0, None, f"Unknown error: {e}"
+        return {}, f"{e}"
 
 
 async def send_uid_info_to_jugo(session: aiohttp.ClientSession, data: List[Dict]):
@@ -139,21 +50,20 @@ async def send_uid_info_to_jugo(session: aiohttp.ClientSession, data: List[Dict]
     logger.error(f"Failed sending to jugo: {text}")
 
 
-async def get_infos(uid, session, hotkey, axon):
-    weight, gpus, err = await get_gpus(session, hotkey, axon)
-    models = await get_models(session, hotkey, axon)
+async def get_infos(
+    uid, session, hotkey, axon
+) -> Tuple[int, Any, Dict[str, int], Dict, str]:
+    models, err = await get_models(session, hotkey, axon)
     jugo_info = {
         "uid": uid,
         "data": {
             "miner_cache": {
-                "weight": weight,
-                "nodes_endpoint_error": err,
+                "models_error": err,
                 "models": models,
-                "gpus": gpus,
             },
         },
     }
-    return uid, axon, weight, models, jugo_info, err
+    return uid, axon, models, jugo_info, err
 
 
 async def sync_miners():
@@ -169,7 +79,7 @@ async def sync_miners():
     miner_models = {}
 
     tasks = []
-    task_responses = []
+    task_responses: List[Tuple[int, Any, Dict[str, int], Dict, str]] = []
     async with aiohttp.ClientSession(
         timeout=aiohttp.ClientTimeout(total=20)
     ) as session:
@@ -188,26 +98,26 @@ async def sync_miners():
             tasks = []
 
     jugo_info_list = []
-    for uid, axon, weight, models, jugo_info, err in task_responses:
+    for uid, axon, models, jugo_info, err in task_responses:
         jugo_info_list.append(jugo_info)
-        redis_miner_data = {
-            "ip": axon.ip,
-            "port": axon.port,
-            "hotkey": axon.hotkey,
-            "coldkey": axon.coldkey,
-            "uid": uid,
-            "weight": weight,
-        }
-        logger.info(redis_miner_data)
         if err != None:
             logger.error(err)
-        for model in models:
+        for model, qps in models.items():
+            redis_miner_data = {
+                "ip": axon.ip,
+                "port": axon.port,
+                "hotkey": axon.hotkey,
+                "coldkey": axon.coldkey,
+                "uid": uid,
+                "weight": qps,
+            }
+            logger.info(redis_miner_data)
             if miner_models.get(model) is None:
                 miner_models[model] = []
             miner_models[model].append(redis_miner_data)
 
     async with aiohttp.ClientSession(
-        timeout=aiohttp.ClientTimeout(total=20)
+        timeout=aiohttp.ClientTimeout(total=5)
     ) as session:
         await send_uid_info_to_jugo(session, jugo_info_list)
     for model in miner_models.keys():
