1dd6b398684f67d4eb01aa91a3b25068a2d20d80
Ben <44178664+cxmplex@users.noreply.github.com>
2025-03-13 20:21:43 -0400

    fix success rate (#24)
    * fix success rate
    
    * Update query_miner.go

--- api/internal/bittensor/query_miner.go
+++ api/internal/bittensor/query_miner.go
@@ -42,6 +42,7 @@ type MinerSuccessRates struct {
 	CompletedOverTime   []int     `json:"completedOverTime"`
 	Attempted           int       `json:"attempted"`
 	Partial             int       `json:"partial"`
+	InFlight            int       `json:"inFlight"`
 	SuccessRateOverTime []float32 `json:"successRateOverTime"`
 	AvgSuccessRate      float32   `json:"avgSuccessRate"`
 	LastReset           time.Time `json:"lastReset"`
@@ -106,9 +107,8 @@ func ReportStats(public string, private string, hotkey string, logger *zap.Sugar
 		for _, v := range minerSuccessRatesMap {
 			v.mu.Lock()
 			rate := float32(1)
-			if v.Attempted != 0 {
-				rate = float32(v.Completed) / float32(v.Attempted)
-				rate = min(rate, 1)
+			if v.Attempted > 0 && v.Attempted > v.InFlight {
+			    rate = min(float32(v.Completed)/float32(v.Attempted-v.InFlight), 1)
 			}
 			v.SuccessRateOverTime = append(v.SuccessRateOverTime, rate)
 			if len(v.SuccessRateOverTime) > 10 {
@@ -120,8 +120,9 @@ func ReportStats(public string, private string, hotkey string, logger *zap.Sugar
 			if len(v.CompletedOverTime) > 10 {
 				v.CompletedOverTime = v.CompletedOverTime[1:]
 			}
+			
 			v.Completed = 0
-			v.Attempted = 0
+			v.Attempted = v.InFlight
 			v.Partial = 0
 			v.LastReset = time.Now()
 			v.mu.Unlock()
@@ -326,6 +327,7 @@ func QueryMiner(c *shared.Context, req *shared.RequestInfo) (*shared.ResponseInf
 	m := minerSuccessRatesMap[miner.Uid]
 	m.mu.Lock()
 	m.Attempted++
+	m.InFlight++
 	m.mu.Unlock()
 	globalStats.mu.Lock()
 	globalStats.AttemptedCurrent++
@@ -495,6 +497,7 @@ func QueryMiner(c *shared.Context, req *shared.RequestInfo) (*shared.ResponseInf
 		case <-c.Request().Context().Done():
 			minerSuccessRatesMap[miner.Uid].mu.Lock()
 			minerSuccessRatesMap[miner.Uid].Attempted = max(minerSuccessRatesMap[miner.Uid].Attempted-1, 0)
+			minerSuccessRatesMap[miner.Uid].InFlight--
 			minerSuccessRatesMap[miner.Uid].mu.Unlock()
 			responseInfo.Error = "user canceled request"
 			return responseInfo, nil
@@ -503,6 +506,7 @@ func QueryMiner(c *shared.Context, req *shared.RequestInfo) (*shared.ResponseInf
 		}
 		minerSuccessRatesMap[miner.Uid].mu.Lock()
 		minerSuccessRatesMap[miner.Uid].Partial++
+		minerSuccessRatesMap[miner.Uid].InFlight--
 		minerSuccessRatesMap[miner.Uid].mu.Unlock()
 		responseInfo.Error = "Premature end of generation"
 		return responseInfo, nil
@@ -516,6 +520,7 @@ func QueryMiner(c *shared.Context, req *shared.RequestInfo) (*shared.ResponseInf
 	)
 	minerSuccessRatesMap[miner.Uid].mu.Lock()
 	minerSuccessRatesMap[miner.Uid].Completed++
+	minerSuccessRatesMap[miner.Uid].InFlight--
 	minerSuccessRatesMap[miner.Uid].mu.Unlock()
 	return responseInfo, nil
 }
